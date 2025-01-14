75930982f0bba51ff2018c73adbe4db2ae0cf0cb
Terry Barillon <97261063+Many0nne@users.noreply.github.com>
2025-01-14 15:02:46 +0100

    Delete backend directory

--- backend/middleware/index.js
+++
@@ -1,5 +0,0 @@
-// This file exports middleware functions that can be used in the application, such as authentication or logging middleware.
-module.exports = (req, res, next) => {
-    console.log('Middleware exécuté');
-    next();
-};
