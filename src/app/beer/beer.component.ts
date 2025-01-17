0b692147285a99c5882373d0ded1d9d91f939034
Manyonne <ptitesailes@gmail.com>
2025-01-17 08:51:22 +0100

    try updating

--- src/app/beer/beer.component.ts
+++ src/app/beer/beer.component.ts
@@ -1,55 +1,54 @@
-import { Component } from '@angular/core';
-import { BeerService } from '../services/beer.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from "../navbar/navbar.component";
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
+import { BeerService } from '../services/beer.service';
 import { BreweryService } from '../services/brewery.service';
-
-
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-beer',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatSelectModule],
+  imports: [CommonModule, ReactiveFormsModule, NavbarComponent, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './beer.component.html',
-  styleUrl: './beer.component.scss'
+  styleUrls: ['./beer.component.scss']
 })
-export class BeerComponent {
+export class BeerComponent implements OnInit {
   beers: any[] = [];
   filteredBeers: any[] = [];
   breweries: any[] = [];
   beerForm: FormGroup;
 
-  constructor(private beerService: BeerService, private fb: FormBuilder, private BreweryService: BreweryService) {
+  constructor(private beerService: BeerService, private fb: FormBuilder, private breweryService: BreweryService) {
     this.beerForm = this.fb.group({
       name: [''],
       description: [''],
       price: [''],
       brewery_id: ['']
-    })
+    });
   }
 
   ngOnInit(): void {
-    this.beerService.getBeers().subscribe(data => {
-      this.beers = data;
-      this.filteredBeers = data;
-      console.log('Beers:', this.beers); // Ajoutez ce console.log pour vérifier les données
+    this.beerService.getBeers().subscribe(response => {
+      this.beers = response.data; // Accédez à la propriété 'data' de la réponse
+      this.filteredBeers = this.beers;
+      console.log('Beers:', this.beers); // Vérifiez les données dans la console
     }, error => {
-      console.error('Error fetching beers:', error); // Ajoutez ce console.log pour vérifier les erreurs
+      console.error('Error fetching beers:', error); // Vérifiez les erreurs dans la console
     });
 
-    this.BreweryService.getBreweries().subscribe(data => {
+    this.beerService.getBreweries().subscribe(data => {
       this.breweries = data;
       console.log('Breweries:', this.breweries); // Vérifiez les données dans la console
     }, error => {
       console.error('Error fetching breweries:', error); // Vérifiez les erreurs dans la console
     });
 
-
+    // Demander explicitement les brasseries
+    this.breweryService.fetchBreweries();
   }
 
   onSearch(event: Event): void {
@@ -70,5 +69,4 @@ export class BeerComponent {
       });
     }
   }
-
 }
